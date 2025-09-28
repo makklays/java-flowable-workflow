f12142c3fe42ea4651cc96cf872f9333dd8e4629
Alexander <alexander@makklays.com.ua>
2025-09-28 16:03:39 +0200

    28-09-2025 Add new dependency with Flyway and first migration 'create_users_table'.

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,33 @@
+
+-- V1__create_users_table.sql
+-- Migration #1: create table users
+
+-- Creating table 'users' for saving main data of users
+CREATE TABLE users
+(
+    id          bigint auto_increment primary key,
+    username    varchar(255) not null,
+
+    firstname   varchar(255) null,
+    lastname    varchar(255) null,
+
+    email       varchar(200) not null,
+    phone       varchar(255) null,
+
+    is_man      TINYINT(1) NOT NULL DEFAULT 0;
+    age         varchar(255) null,
+
+    password    varchar(255) null,
+    created_at  datetime(6)  null,
+    updated_at  datetime(6)  null,
+
+    constraint UK_email unique (email),
+    constraint UK_username unique (username)
+);
+
+-- Adding index for fast searching by email
+CREATE INDEX idx_users_email ON users(email);
+
+-- Adding index for fast searching by mobile
+CREATE INDEX idx_users_mob ON users(mob);
+

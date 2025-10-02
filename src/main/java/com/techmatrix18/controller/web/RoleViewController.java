3bcd17b8d689bcfb1e8d6e8b69c9f7dd80f500d0
Alexander <alexander@makklays.com.ua>
2025-10-02 22:09:22 +0200

    02-10-2025 Add new pages and design for an entity 'Roles'.

--- src/main/java/com/techmatrix18/controller/web/RoleViewController.java
+++ src/main/java/com/techmatrix18/controller/web/RoleViewController.java
@@ -1,8 +1,12 @@
 package com.techmatrix18.controller.web;
 
+import com.techmatrix18.model.Role;
+import com.techmatrix18.service.RoleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.logging.Logger;
 
 /**
@@ -19,10 +23,44 @@ public class RoleViewController {
 
     Logger log = Logger.getLogger(RoleViewController.class.getName());
 
+    private final RoleService roleService;
+
+    public RoleViewController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
     @GetMapping("/roles")
-    public String welcome(Model model) {
+    public String getAllRoles(Model model) {
 
         return "roles/index";
+    }
+
+    @GetMapping("/roles/add")
+    public String addRole(Model model) {
+
+        return "roles/add";
+    }
+
+    @GetMapping("/roles/edit/{id}")
+    public String editRole(@PathVariable Long id, Model model) {
+        Role role = roleService.getById(id);
+        model.addAttribute("role", role);
+
+        return "roles/edit";
+    }
+
+    @GetMapping("/roles/view/{id}")
+    public String viewRole(@PathVariable Long id, Model model) {
+        Role role = roleService.getById(id);
+        model.addAttribute("role", role);
+
+        return "roles/view";
+    }
+
+    @GetMapping("/roles/delete/{id}")
+    public void deleteRole(@PathVariable Long id, Model model) {
+        Role role = roleService.getById(id);
+        //
     }
 }
 

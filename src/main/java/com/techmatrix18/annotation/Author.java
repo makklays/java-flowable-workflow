bd31adc25af8cd77cb3998a7de70875dcffe360d
Alexander <alexander@makklays.com.ua>
2025-10-07 13:17:51 +0200

    07-10-2025 Add new annotations.

---
+++ src/main/java/com/techmatrix18/annotation/Author.java
@@ -0,0 +1,14 @@
+package com.techmatrix18.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Author {
+    String name();
+    String date() default "07-10-2025";
+}
+

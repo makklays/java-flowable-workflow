d6b76d8d14bf9f2924dc7d35ac4a90ed08bb34e6
Alexander <alexander@makklays.com.ua>
2025-10-24 16:25:06 +0200

    24-10-2025 Add new class with variables (in stack and heap memory).

---
+++ src/main/java/com/techmatrix18/patterns/Variables.java
@@ -0,0 +1,20 @@
+package com.techmatrix18.patterns;
+
+/**
+ * This is Variables class
+ *
+ * @company for TechMatrix18
+ * @author Alexander Kuziv
+ * @since 24-10-2025
+ * @version 0.0.1
+ */
+
+public class Variables {
+    public static void main(String[] args) {
+        int i = 3; // stack memory
+        long l = 33L; // stack memory
+        String s = "3"; // heap memory (object)
+        Object o = new Object(); // heap memory (object)
+    }
+}
+

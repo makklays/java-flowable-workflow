ce481de01a271663a7cb40480e70e224a6ab768c
Alexander <alexander@makklays.com.ua>
2025-10-28 12:41:52 +0100

    28-10-2025 Add new interface 'Comparable' and compareTo(Person other); Comparator<T>; .

--- src/main/java/com/techmatrix18/patterns/PersonComparable.java
+++ src/main/java/com/techmatrix18/patterns/PersonComparable.java
@@ -31,3 +31,28 @@ public class PersonComparable implements Comparable<PersonComparable> {
     }
 }
 
+
+/*
+List<Person> people = List.of(
+        new Person("Alex", 30),
+        new Person("Maria", 25),
+        new Person("John", 40)
+);
+
+List<Person> sorted = new ArrayList<>(people);
+Collections.sort(sorted); // использует compareTo()
+
+System.out.println(sorted); // [Maria (25), Alex (30), John (40)]
+*/
+
+
+/*
+Comparator<T> - MANY caracteristics
+
+Comparator<Person> byName = (p1, p2) -> p1.getName().compareTo(p2.getName());
+Collections.sort(people, byName);
+
+Comparator<Person> byAge = (p1, p2) -> Integer.compare(p2.getAge(), p1.getAge());
+Collections.sort(people, byAge);
+*/
+

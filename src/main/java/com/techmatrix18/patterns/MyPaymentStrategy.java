13cea110157ebc742ad00937beb212d676a3dff5
Alexander <alexander@makklays.com.ua>
2025-11-02 19:20:58 +0100

    02-11-2025 Add a new pattern 'Strategy' (for processing payments).

---
+++ src/main/java/com/techmatrix18/patterns/MyPaymentStrategy.java
@@ -0,0 +1,63 @@
+package com.techmatrix18.patterns;
+
+/**
+ * "Strategy" - Pattern
+ * for processing payments
+ *
+ * @author Alexander Kuziv
+ * @since 02.11.2025
+ * @company TechMatrix18
+ * @version 0.0.1
+ */
+
+// Common interface for all strategies
+interface PaymentStrategy {
+    void pay(double amount);
+}
+
+public class MyPaymentStrategy {
+    private PaymentStrategy paymentStrategy;
+
+    // Can change strategy at any time
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void pay(double amount) {
+        if (paymentStrategy == null) {
+            throw new IllegalStateException("Payment strategy is not set!");
+        }
+        paymentStrategy.pay(amount);
+    }
+}
+
+class CreditCardPayment implements PaymentStrategy {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using Credit Card.");
+    }
+}
+
+class PayPalPayment implements PaymentStrategy {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using PayPal.");
+    }
+}
+
+/*
+public class StrategyExample {
+    public static void main(String[] args) {
+        PaymentContext context = new PaymentContext();
+
+        // Оплата кредиткой
+        context.setPaymentStrategy(new CreditCardPayment());
+        context.pay(100.0);
+
+        // Меняем стратегию во время выполнения
+        context.setPaymentStrategy(new PayPalPayment());
+        context.pay(50.0);
+    }
+}
+*/
+

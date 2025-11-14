4314d35277e327a85874a1948a80075671350651
Alexander <alexander@makklays.com.ua>
2025-11-14 21:47:47 +0100

    14-11-2025 Add a new 'ThreadPoolExecutor', 'Task2'.

---
+++ src/main/java/com/techmatrix18/mythreads/MyThreadPoolExecutor.java
@@ -0,0 +1,62 @@
+package com.techmatrix18.mythreads;
+
+import java.util.concurrent.*;
+
+/**
+ * My Thread Pool Executor - пример использования ThreadPoolExecutor
+ *
+ * @author Alexander Kuziv
+ * @since 14.11.2025
+ * @company TechMatrix18
+ * @version 0.0.1
+ */
+
+public class MyThreadPoolExecutor {
+    public static void main(String[] args) {
+
+        // Создаём ThreadPoolExecutor вручную
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                2,            // corePoolSize — всегда активные потоки
+                4,                       // maximumPoolSize — максимум потоков
+                10, TimeUnit.SECONDS,    // время жизни доп. потоков
+                new ArrayBlockingQueue<>(2), // очередь на 2 задачи
+                new ThreadPoolExecutor.AbortPolicy() // политика отказа
+        );
+
+        // Отправляем 10 задач
+        for (int i = 1; i <= 10; i++) {
+            int taskId = i;
+
+            try {
+                executor.execute(() -> {
+                    System.out.println("Поток " + Thread.currentThread().getName()
+                            + " выполняет задачу " + taskId);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                });
+            } catch (RejectedExecutionException e) {
+                System.out.println("❌ Задача " + taskId + " была отклонена!");
+            }
+
+        }
+
+        executor.shutdown();
+    }
+}
+
+/*
+❌ Задача 7 была отклонена!
+❌ Задача 8 была отклонена!
+❌ Задача 9 была отклонена!
+❌ Задача 10 была отклонена!
+Поток pool-1-thread-1 выполняет задачу 1
+Поток pool-1-thread-2 выполняет задачу 2
+Поток pool-1-thread-4 выполняет задачу 6
+Поток pool-1-thread-3 выполняет задачу 5
+Поток pool-1-thread-1 выполняет задачу 4
+Поток pool-1-thread-2 выполняет задачу 3
+*/
+
---
+++ src/main/java/com/techmatrix18/mythreads/Task2.java
@@ -0,0 +1,44 @@
+package com.techmatrix18.mythreads;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * Task2 class — Техзадание №2 — “Конкурентная обработка заказов с использованием пула потоков и сбором результатов”
+ *
+ * @author Alexander Kuziv
+ * @since 14.11.2025
+ * @company TechMatrix18
+ * @version 0.0.1
+ */
+
+public class Task2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        final List<Integer> myList = new ArrayList<Integer>();
+
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for(int i = 1; i <= 5; i++) {
+            int number = i;
+
+            Runnable r = new Runnable() {
+                public void run() {
+                    synchronized (myList) {
+                        for(int i = 1; i <= 1000; i++) {
+                            myList.add(number);
+                        }
+                    }
+                }
+            };
+            executor.submit(r);
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
+
+        System.out.println("Размер: " + myList.size());
+    }
+}
+

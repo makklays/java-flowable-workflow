56f8a58bd5d66f6d115bab3d030c02e13c482682
Alexander <alexander@makklays.com.ua>
2026-04-29 23:18:09 +0200

    29-04-2026 Add a new React components for forms.

--- frontend/src/pages/flowables/TaskFormPage.js
+++ frontend/src/pages/flowables/TaskFormPage.js
@@ -1,26 +1,18 @@
-import ReviewForm from './forms/ReviewForm';
-import SimpleApproveForm from './forms/SimpleApproveForm';
+import { FORM_COMPONENTS } from './flowable-forms';
 
 const TaskFormPage = () => {
-    const { taskId } = useParams();
     const location = useLocation();
     const task = location.state?.task;
 
-    // Словарь ваших компонентов
-    const formComponents = {
-        'review-form': <ReviewForm task={task} />,
-        'approve-billing': <SimpleApproveForm task={task} />,
-    };
+    // Получаем нужный компонент из реестра
+    const SelectedForm = FORM_COMPONENTS[task?.formKey];
 
     return (
-        <div className="container mt-4">
-            <h2>Задача: {task?.name}</h2>
-            <hr />
-            {/* Рендерим форму по ключу или заглушку */}
-            {formComponents[task?.formKey] || (
-                <div className="alert alert-warning">
-                    Форма для ключа "{task?.formKey}" не найдена.
-                </div>
+        <div>
+            {SelectedForm ? (
+                <SelectedForm task={task} />
+            ) : (
+                <div className="alert alert-danger">Компонент для {task?.formKey} не создан</div>
             )}
         </div>
     );
